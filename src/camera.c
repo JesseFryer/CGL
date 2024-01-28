7c8e9d0372d8c72a24774a533c9bad37b6c8adc2
JesseFryer <jesse.fryer10@gmail.com>
2024-01-28 14:17:04 +1000

    fixed bug in camera.

--- src/camera.c
+++ src/camera.c
@@ -12,8 +12,8 @@ void cam_init_camera(Camera* cam,
     cam->up[1] = 1.0f;
     cam->up[2] = 0.0f;
     cam->front[0] = 0.0f;
-    cam->front[0] = 0.0f;
-    cam->front[1] = -1.0f;
+    cam->front[1] = 0.0f;
+    cam->front[2] = -1.0f;
     cam->yaw = -90.0f;
     cam->pitch = 0.0f;
 }
@@ -27,9 +27,9 @@ void cam_move(Camera* cam, clmVec3 move) {
 
 void cam_view_matrix(Camera* cam, clmMat4 view) {
     clmVec3 target = { 
-        cam->position[0],
-        cam->position[1],
-        -1.0f
+        cam->position[0] + cam->front[0],
+        cam->position[1] + cam->front[1],
+        cam->position[2] + cam->front[2]
     };
     clm_mat4_lookat(view,
             cam->position,
